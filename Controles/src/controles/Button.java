7c66284b8b575bcc5a64d01ac7aa768e909ff9d6
LisAgui09 <shirleytoalaagui1999@gmail.com>
2024-06-08 12:11:57 -0500

    Add files via upload

---
+++ Controles/src/controles/Button.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controles;
+
+/**
+ *
+ * @author Admin
+ */
+class Button {
+
+    Button(String confirmar_selección) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ Controles/src/controles/DatePickerColorPickerApp.java
@@ -0,0 +1,78 @@
+package application;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import javafx.geometry.Pos;
+
+public class CONTROLES_BASICOS extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Formulario de Controles");
+
+        Button button = new Button("button");
+        CheckBox checkBox = new CheckBox("CheckBox");
+        Hyperlink hyperlink = new Hyperlink("Hyperlink");
+        ToggleButton toggleButton = new ToggleButton("ToggleButton");
+        RadioButton radioButton = new RadioButton("RadioButton");
+        Label label = new Label("Label");
+        TextField textField = new TextField();
+        PasswordField passwordField = new PasswordField();
+        TextArea textArea = new TextArea();
+        ProgressIndicator progressIndicator = new ProgressIndicator();
+        ProgressBar progressBar = new ProgressBar();
+        Slider slider = new Slider();
+
+        GridPane gridPane = new GridPane();
+        gridPane.setAlignment(Pos.CENTER);
+        gridPane.setVgap(10);
+        gridPane.setHgap(10);
+
+        gridPane.add(new Label("Button:"), 0, 0);
+        gridPane.add(button, 1, 0);
+
+        gridPane.add(new Label("Checkbox:"), 0, 1);
+        gridPane.add(checkBox, 1, 1);
+
+        gridPane.add(new Label("Hyperlink:"), 0, 2);
+        gridPane.add(hyperlink, 1, 2);
+
+        gridPane.add(new Label("ToggleButton:"), 0, 3);
+        gridPane.add(toggleButton, 1, 3);
+
+        gridPane.add(new Label("RadioButton:"), 0, 4);
+        gridPane.add(radioButton, 1, 4);
+
+        gridPane.add(new Label("Label:"), 0, 5);
+        gridPane.add(label, 1, 5);
+
+        gridPane.add(new Label("TextField:"), 0, 6);
+        gridPane.add(textField, 1, 6);
+
+        gridPane.add(new Label("PasswordField:"), 0, 7);
+        gridPane.add(passwordField, 1, 7);
+
+        gridPane.add(new Label("TextArea:"), 0, 8);
+        gridPane.add(textArea, 1, 8);
+
+        gridPane.add(new Label("ProgressIndicator:"), 0, 9);
+        gridPane.add(progressIndicator, 1, 9);
+
+        gridPane.add(new Label("ProgressBar:"), 0, 10);
+        gridPane.add(progressBar, 1, 10);
+
+        gridPane.add(new Label("Slider:"), 0, 11);
+        gridPane.add(slider, 1, 11);
+
+        Scene scene = new Scene(gridPane, 400, 600);
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
